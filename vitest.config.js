985a868010d9bf1af30a19af6fe5c8f48dd8b3cf
Dan Fabulich <dan@fabulich.com>
2025-05-13 03:32:27 -0700

    remove vitest; we don't have unit tests for shaders

--- vitest.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-  test: {
-    // environment: 'jsdom', // Uncomment if your tests need DOM or browser APIs
-    // globals: true, // If you want to use describe, test, expect globally without importing
-  },
-});
